88ca73ea2c77702a70ed64becd793055c3e01da0
rayed168 <muhammad.rayed2013@gmail.com>
2025-01-14 17:02:17 +0200

    First commit

---
+++ intro to dictonaries.py
@@ -0,0 +1,17 @@
+my_dict = {}
+
+my_dict = {1 : "apple", "fruit" : "banana", 3 : "Rayed"}
+
+print(my_dict[3])
+print(my_dict[1])
+print(my_dict["fruit"])
+
+my_dict["fruit"] = "apple"
+print(my_dict)
+
+my_dict.pop("fruit")
+
+print(my_dict)
+
+my_dict.clear
+print(my_dict)
